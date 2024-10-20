87106967a72bc994c4db251de9a27c1bcaa413ed
tai-kun <taikun.dev@gmail.com>
2024-10-20 15:20:25 +0900

    スクリプトを改善

--- tags.sh
+++ tags.sh
@@ -2,8 +2,13 @@
 
 set -euo pipefail
 
+tag_prefix="${TAG_PREFIX:-}"
+tag_suffix="${TAG_SUFFIX:-}"
+
 echo "$CSV_LIST" |
     tr ',' '\n' |
     sed 's/^[ \t]*//;s/[ \t]*$//' |
-    grep -v '^$' | sed "s|^|ghcr.io/$REPO_OWNER/surrealdb:|" |
+    grep -v '^$' | sed "s|^|$tag_prefix|" |
+    grep -v '^$' | sed "s|$|$tag_suffix|" |
+    grep -v '^$' | sed "s|^|ghcr.io/$REPO_OWNER/$PACKAGE:|" |
     paste -sd ',' -
